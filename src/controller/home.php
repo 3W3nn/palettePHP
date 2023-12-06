bcf563541bec4ce2e294156a9eb5307a6e524f21
3W3nn <126698220+3W3nn@users.noreply.github.com>
2023-12-06 10:07:51 +0100

    fix bug homepage

--- src/controller/home.php
+++ src/controller/home.php
@@ -4,5 +4,11 @@
 
 use Symfony\Component\HttpFoundation\Response;
 
+<<<<<<< Updated upstream
 return new Response($twig->render('home/home.html.twig'));
+=======
+$folder = 'assets/image/logo.png';
+
+return new Response($twig->render('splashscreen.html.twig', ['logo' => $folder]));
+>>>>>>> Stashed changes
 
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,1 +1,13 @@
 <?php
+<<<<<<< Updated upstream
+=======
+
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+$routes = new RouteCollection();
+
+$routes->add('home', new Route('/home'));
+
+return $routes;
+>>>>>>> Stashed changes
