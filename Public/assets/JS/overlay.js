af993bbadae628959ce7e5982434e4e5ac42ebc7
3W3nn <126698220+3W3nn@users.noreply.github.com>
2023-12-25 12:01:22 +0100

    🐛fixing bug in overlay menu

--- Public/assets/JS/overlay.js
+++ Public/assets/JS/overlay.js
@@ -1,26 +1,34 @@
 
     // Fonction pour ouvrir le menu overlay
     function openNav() {
-    document.getElementById("ov_menu").style.width = "25%" // Ou la largeur que vous voulez
-}
+        var overlay = document.getElementById("ov_menu");
+        var zoneCliquable = document.getElementById("zoneCliquable");
+        overlay.style.width = "25%";
+        overlay.classList.add("active"); // Ajoute une classe lorsque l'overlay est ouvert
+        zoneCliquable.style.display = "block"; // Affiche la zone cliquable
+    }
+
 
     // Fonction pour fermer le menu overlay
     function closeNav() {
-    document.getElementById("ov_menu").style.width = "0%";
-}
+        var overlay = document.getElementById("ov_menu");
+        var zoneCliquable = document.getElementById("zoneCliquable");
+        overlay.style.width = "0";
+        overlay.classList.remove("active"); // Supprime la classe lorsque l'overlay est fermé
+        zoneCliquable.style.display = "none"; // Cache la zone cliquable
+    }
 
     // Ajoute un écouteur d'événements pour fermer l'overlay lorsque l'on clique en dehors de celui-ci
     window.addEventListener('click', function(event) {
-    var overlay = document.getElementById("ov_menu");
+    //var overlay = document.getElementById("ov_menu");
     var zoneCliquable = document.getElementById('zoneCliquable');
-
+    var overlay_content = document.querySelector('.overlay-content');
+    //var menu  = document.getElementById('zoneCliquable');
     // Vérifie si le clic était sur l'overlay (mais pas sur son contenu)
-    if (event.target === overlay) {
-    closeNav();
-}
-    if (event.target === zoneCliquable) {
-    closeNav();
-}
+        if (event.target === zoneCliquable) {
+            closeNav();
+        }
+
 });
 
 
